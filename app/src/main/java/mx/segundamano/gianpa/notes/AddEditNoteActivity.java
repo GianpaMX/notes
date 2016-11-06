2d3e06f5bc92e327b1166d617037839a09848fe1
Juan Villa <juan64@gmail.com>
2016-11-06 17:19:30 -0600

    Add/Edit note UI

--- app/src/main/java/mx/segundamano/gianpa/notes/AddEditNoteActivity.java
+++ app/src/main/java/mx/segundamano/gianpa/notes/AddEditNoteActivity.java
@@ -2,6 +2,8 @@ package mx.segundamano.gianpa.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class AddEditNoteActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class AddEditNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_edit_note_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_edit_note_activity, menu);
+        return true;
     }
 }
