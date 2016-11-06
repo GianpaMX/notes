b7c58bde8b6fea26addef0599e8634f594d95631
Juan Villa <juan64@gmail.com>
2016-11-06 16:40:51 -0600

    first commit

---
+++ app/src/main/java/mx/segundamano/gianpa/notes/AddEditNoteActivity.java
@@ -0,0 +1,13 @@
+package mx.segundamano.gianpa.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddEditNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_edit_note_activity);
+    }
+}
