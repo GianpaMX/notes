0026aca5e8050de18b3303b6010f12cd1bfe746a
Juan Villa <juan64@gmail.com>
2016-11-07 02:47:13 -0600

    Moving dependencies from Model to Presenter

--- app/src/main/java/mx/segundamano/gianpa/notes/addeditnote/AddEditNotePresenter.java
+++ app/src/main/java/mx/segundamano/gianpa/notes/addeditnote/AddEditNotePresenter.java
@@ -1,13 +1,26 @@
 package mx.segundamano.gianpa.notes.addeditnote;
 
+import io.realm.Realm;
 import mx.segundamano.gianpa.notes.Note;
+import mx.segundamano.gianpa.notes.NotesRepository;
+import mx.segundamano.gianpa.notes.NotesService;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddEditNotePresenter implements AddEditNoteModelCallback {
     private AddEditNoteView view;
     private AddEditNoteModel model;
 
     public AddEditNotePresenter(AddEditNoteView view) {
-        this.model = new AddEditNoteModel();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.myjson.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        NotesService service = retrofit.create(NotesService.class);
+        Realm realm = Realm.getDefaultInstance();
+        NotesRepository gateway = new NotesRepository(service, realm);
+
+        this.model = new AddEditNoteModel(gateway);
         this.view = view;
     }
 
